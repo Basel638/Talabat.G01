cedc61e3832135bcd71e54d6ed7dae3c47ac1ad2
Basel638 <basselsameh2004@gmail.com>
2024-04-11 16:30:24 +0200

    Part 10 Update Database

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -5,10 +5,13 @@ namespace Talabat.APIs
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
+
+			
 			var webApplicationBuilder = WebApplication.CreateBuilder(args);
 
+			#region Configure Services
 			// Add services to the container.
 
 			webApplicationBuilder.Services.AddControllers();
@@ -22,8 +25,35 @@ namespace Talabat.APIs
 				options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
 			});
 
+			#endregion
+
+			  
 			var app = webApplicationBuilder.Build();
 
+
+			using var scope = app.Services.CreateScope();
+
+
+			var services = scope.ServiceProvider;
+			
+			var _dbContext = services.GetRequiredService<StoreContext>();
+			// ASK CLR for Creating Object from DbContext Explicitly
+
+
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();	
+
+			try
+			{
+				await _dbContext.Database.MigrateAsync(); // Update-Database
+
+			}
+			catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(ex, "an Error has been occured during apply the migration");
+
+            }
+			#region Configure Kestrel Middlewares
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
@@ -36,7 +66,8 @@ namespace Talabat.APIs
 			app.UseAuthorization();
 
 
-			app.MapControllers();
+			app.MapControllers(); 
+			#endregion
 
 			app.Run();
 		}
