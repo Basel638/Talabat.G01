654af88abb5e00f6f2443c3ccd1cf5a6074b9dc3
Basel638 <basselsameh2004@gmail.com>
2024-04-18 04:22:52 +0200

    Part 04 Employee Example - Draft

---
+++ Talabat.APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat_Core.Entities;
+using Talabat_Core.Repositories.Contract;
+using Talabat_Core.Specifications.EmployeeSpecs;
+
+namespace Talabat.APIs.Controllers
+{
+
+	public class EmployeeController : BaseApiController
+	{
+		private readonly IGenericRepository<Employee> _employeesRepo;
+
+		public EmployeeController(IGenericRepository<Employee> employeesRepo)
+        {
+			_employeesRepo = employeesRepo;
+		}
+
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+		{
+			var spec = new EmployeeWithDepartmentSpecifications();
+
+			var employees = await _employeesRepo.GetAllWithSpecAsync(spec);
+
+			return Ok(employees);
+		}
+
+		[HttpGet("{id}")]
+
+		public async Task<ActionResult<Employee>> GetEmployee(int id)
+		{
+			var spec = new EmployeeWithDepartmentSpecifications(id);
+
+			var employee = await _employeesRepo.GetWithSpecAsync(spec);
+			if(employee is null) 
+				return NotFound();
+
+			return Ok(employee);
+
+		}
+	}
+}
---
+++ Talabat_Core/Specifications/EmployeeSpecs/EmployeeWithDepartmentSpecifications.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat_Core.Entities;
+
+namespace Talabat_Core.Specifications.EmployeeSpecs
+{
+	public class EmployeeWithDepartmentSpecifications:BaseSpecificatoins<Employee>
+	{
+
+
+        public EmployeeWithDepartmentSpecifications()
+        :base()
+        {
+            Includes.Add(E => E.Department);
+        }
+
+
+        public EmployeeWithDepartmentSpecifications(int id):base(E => E.Id==id)
+        {
+            Includes.Add(E => E.Department);
+
+		}
+
+	}
+}
