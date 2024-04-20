176ddb5b27dc318c5e4f4f68efcc166aefec5a38
Basel638 <basselsameh2004@gmail.com>
2024-04-20 14:46:03 +0200

    Part 10 Validation Error Handling

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Talabat.APIs.Errors;
 using Talabat.APIs.Helpers;
 using Talabat_Core.Entities;
 using Talabat_Core.Repositories.Contract;
@@ -32,6 +34,26 @@ namespace Talabat.APIs
 			webApplicationBuilder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 			
 			webApplicationBuilder.Services.AddAutoMapper(typeof(MappingProfiles));
+
+			webApplicationBuilder.Services.Configure<ApiBehaviorOptions>(options =>
+			{
+				options.InvalidModelStateResponseFactory = (actionContext) =>
+				{
+
+					var errors = actionContext.ModelState.Where(P => P.Value.Errors.Count() > 0)
+													.SelectMany(P => P.Value.Errors)
+													.Select(E => E.ErrorMessage)
+													.ToList();
+
+					var response = new ApiValidationErrorResponse()
+					{
+						Errors = errors
+					};
+
+					return new BadRequestObjectResult(response);
+				};
+
+			});
 
 			#endregion
 
